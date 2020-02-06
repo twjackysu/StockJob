f89dfa9b47e98a30cd08ccd877fb037aeb92cad3
JackySu <jacky19918@gmail.com>
2020-02-06 21:05:53 +0800

    加入專案檔案。

---
+++ StockJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
